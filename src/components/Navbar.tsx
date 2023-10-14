9a6b9f0c318e7ffdfc3f99ed5cc59deafaeb91d3
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-10-13 21:36:13 -0500

    update navbvar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions)
   return (
     <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
-      <div className='container max-w-7xl h-full mx-auto flex flex-col sm:flex-row items-center justify-between gap-2'>
+      <div className='h-full mx-auto flex items-center justify-between gap-2'>
         {/* logo */}
         <Link href='/' className='flex gap-2 items-center'>
           <Box className='h-8 w-8 sm:h-6 sm:w-6' />
